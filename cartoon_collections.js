087dd5264496d2983961b0968cf2cc87acd7bef7
charleneclee <cclee@google.com>
2017-05-11 15:09:33 -0700

    argh cartoon collection

--- cartoon_collections.js
+++ cartoon_collections.js
@@ -1,15 +1,41 @@
 function dwarfRollCall(dwarves) {
-	//your code here!
+var rollCall = ""
+  dwarves.forEach(function (name, index) {
+  var number = index +1;
+rollCall = rollCall + number + ". " + name + " "
+});
+return rollCall
 }
 
 function summonCaptainPlanet(planeteerCalls){
-	//your code here!
+var elements = []
+planeteerCalls.forEach(function (element) {
+  var x = element.toUpperCase() + "!"
+  elements.push(x)
+})
+return elements
 }
 
 function longPlaneteerCalls(words) {
-	//your code here!
+  var trueOrFalse = false
+  words.forEach (function (word){
+    if (word.length >= 4){
+      trueOrFalse = true
+    }
+  })
+  return trueOrFalse
 }
 
+
 function findTheCheese(foods) {
-	// your code here!
+  var stringToReturn = "no cheese!"
+  var cheeseList = ["cheddar", "gouda", "camembert"]
+  //examine the list by going through one by one and reading each item on the list to determine if it's a cheese or not a cheese. If it is a cheese, we stop and return the cheese type. If it isn't, we keep going until we find one.
+  foods.forEach(function (item){
+    if (cheeseList.includes(item)) {
+      stringToReturn = item
+    }
+
+  })
+  return stringToReturn
 }
